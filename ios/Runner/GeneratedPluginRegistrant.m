1b4f1a1e06fdcbaf906185ffd715326068458d7d
Steven Schulze <stevenschulze@gmail.com>
2019-03-26 01:18:58 -0700

    revert
    changeback

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,12 +3,10 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-#import <image_picker/ImagePickerPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
 }
 
 @end
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,65 @@
-import 'package:flutter/material.dart';
-import './camera/camera.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyCamera(),
-  ));
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost() async {
+  final response = await http.get('http://stevenschulze.io/json/data.json');
+
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final bool result;
+
+  Post({this.result});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(result: json['result']);
+  }
+}
+
+void main() => runApp(MyApp(post: fetchPost()));
+
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  MyApp({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Result'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data.result) {
+                  return Text("This is a chicken Nugget!");
+                } else {
+                  return Text("This is NOT a chicken Nugget!");
+                }
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
